676295ac7d59bb52714902c2b5d62899a5f04ba2
Sourati <Sourati@DESKTOP-3AF10OJ>
2022-09-09 14:03:18 +0200

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace Loop
+{
+    class ForLoop
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("C# For Loop: Iteration {0}", i);
+            }
+        }
+    }
+}
